fad5944652f9cfaa924e229031463154c6288284
Alvin Dizon <alvin.dizon91@gmail.com>
2017-09-03 11:12:03 +0100

    Change array to ArrayList
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
     private static final String TAG = "NumbersActivity";
 
@@ -13,13 +15,23 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        // create word array
-        String[] words = {"one", "two", "three", "four", "five",
-                    "six", "seven", "eight", "nine", "ten"};
+        // create word arraylist
+        ArrayList<String> words = new ArrayList<>();
+        // add elements in Arraylist
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        // print out each element of the word array to logcat
-        for(int i = 0; i < words.length; i++){
-            Log.v(TAG, "Word at index " + i + ": " + words[i]);
+        // print out each element of the word arraylist to logcat
+        for(int i = 0; i < words.size(); i++){
+            Log.v(TAG, "Word at index " + i + ": " + words.get(i));
         }
 
     }
