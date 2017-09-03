2e3b5dd7f1adb8882c8d6166a7cc0cbd18c361b7
Alvin Dizon <alvin.dizon91@gmail.com>
2017-09-03 14:02:40 +0100

    Add child textviews dynamically
    Each word in the word arrayList should be displayed in a child textview
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -29,10 +31,12 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        // print out each element of the word arraylist to logcat
+        //create a textview showing one of the words, and add it as a child view of NumbersActivity
         for(int i = 0; i < words.size(); i++){
-            Log.v(TAG, "Word at index " + i + ": " + words.get(i));
+            LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
         }
-
     }
 }
