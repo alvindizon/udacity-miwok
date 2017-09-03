503c9010b0022514cc85a1ba0840526d4851f729
Alvin Dizon <alvin.dizon91@gmail.com>
2017-09-03 10:54:45 +0100

    Create word array and print words to logcat
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,10 +5,22 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
+    private static final String TAG = "NumbersActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // create word array
+        String[] words = {"one", "two", "three", "four", "five",
+                    "six", "seven", "eight", "nine", "ten"};
+
+        // print out each element of the word array to logcat
+        for(int i = 0; i < words.length; i++){
+            Log.v(TAG, "Word at index " + i + ": " + words[i]);
+        }
+
     }
 }
