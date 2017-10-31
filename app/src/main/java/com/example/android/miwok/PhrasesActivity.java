7cdfa7f30b24e253c67a5153f5850cd6a2a7c54a
Alvin Dizon <alvin.dizon91@gmail.com>
2017-10-31 14:55:56 +0100

    Removed toast messages
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,7 +20,6 @@ public class PhrasesActivity extends AppCompatActivity {
             new MediaPlayer.OnCompletionListener(){
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
-            Toast.makeText(getApplicationContext(), "I'm done", Toast.LENGTH_SHORT).show();
             releaseMediaPlayer();
         }
     };
